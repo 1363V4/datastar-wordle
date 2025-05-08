0c4dae567b5d3a629fd9c7d86821dc565d86c849
1363v4 <louisemmanuelgires@gmail.com>
2025-05-08 22:06:06 +0200

    HABEMUS PAPAM

---
+++ app.py
@@ -0,0 +1,157 @@
+from quart import Quart, session, render_template, request
+from datastar_py.sse import ServerSentEventGenerator as SSE
+from datastar_py.quart import make_datastar_response
+
+from tinydb import TinyDB
+import redis.asyncio as redis
+
+import asyncio
+from random import choice
+
+from words import words
+
+
+# CONFIG
+
+app = Quart(__name__)
+app.secret_key = 'a_secret_key'
+
+db = TinyDB("data.json", sort_keys=True, indent=2)
+games = db.table('games')
+
+redis_client = redis.Redis(host="localhost", port=6379, db=0, decode_responses=True)
+
+NO_LINES = 5
+
+# UTILS
+
+async def is_game_over(lines):
+    if lines:
+        last_line = [*lines.values()][-1]
+        if all([square[1] == "green" for square in last_line]):
+            return "WIN"
+        elif lines.get(str(NO_LINES-1)):
+            return "LOSE"
+    return "NOPE" 
+
+# VIEWS
+
+async def main_view(lines, current):
+    html = f'''
+    <main 
+    id="main" 
+    class="gc"
+    style="grid-template-rows: repeat({NO_LINES + 1}, 1fr);"
+    data-indicator-fetching
+    data-on-keydown__window="
+    response = react_to_key(evt, $current, $squares);
+    rc = response[0];
+    $current = response[1][0];
+    $squares = [...response[1][1]];
+    rc == 'OK' ? @post('/attempt') : null;"
+    >
+    '''
+    for n in range(NO_LINES):
+        if n == current:
+            html += f'''
+            <div class="line">
+                <div id="sq{n}0" class="square" data-attr-current="$current == 0" data-text="$squares[0]"></div>
+                <div id="sq{n}1" class="square" data-attr-current="$current == 1" data-text="$squares[1]"></div>
+                <div id="sq{n}2" class="square" data-attr-current="$current == 2" data-text="$squares[2]"></div>
+                <div id="sq{n}3" class="square" data-attr-current="$current == 3" data-text="$squares[3]"></div>
+                <div id="sq{n}4" class="square" data-attr-current="$current == 4" data-text="$squares[4]"></div>
+                <div id="sq{n}5" class="square" data-attr-current="$current == 5" data-text="$squares[5]"></div>
+            </div>
+            '''
+        else:
+            line = lines.get(str(n), [["", ""], ["", ""], ["", ""], ["", ""], ["", ""], ["", ""]])
+            html += f'''
+            <div class="line">
+                <div id="sq{n}0" style="animation: rotate .2s linear forwards" class="square {line[0][1]}">{line[0][0]}</div>
+                <div id="sq{n}1" style="animation: rotate .2s linear forwards .4s" class="square {line[1][1]}">{line[1][0]}</div>
+                <div id="sq{n}2" style="animation: rotate .2s linear forwards .8s" class="square {line[2][1]}">{line[2][0]}</div>
+                <div id="sq{n}3" style="animation: rotate .2s linear forwards 1.2s" class="square {line[3][1]}">{line[3][0]}</div>
+                <div id="sq{n}4" style="animation: rotate .2s linear forwards 1.6s" class="square {line[4][1]}">{line[4][0]}</div>
+                <div id="sq{n}5" style="animation: rotate .2s linear forwards 2s" class="square {line[5][1]}">{line[5][0]}</div>
+            </div>
+            '''
+    html += '''
+    <img src="/static/img/bars-fade.svg" class="gc" data-show="$fetching">
+    </main>
+    '''
+    return html
+
+# APP
+
+@app.before_request
+async def before_request():
+    if not session.get('db_id'): 
+        db_id = games.insert({
+            'word': choice(words),
+            'current': 0,
+            'lines': {}
+        })
+        session['db_id'] = db_id
+
+@app.get('/')
+async def index():
+    session.clear()
+    return await render_template('index.html')
+
+@app.get('/main')
+async def main():
+    db_id = session['db_id']
+    pubsub = redis_client.pubsub()
+    await pubsub.subscribe(str(db_id))
+    async def event():
+        while True:
+            try:
+                if await pubsub.get_message():
+                    data = games.get(doc_id=db_id)
+                    lines = data.get('lines')
+                    current = data.get('current')
+                    word = data.get('word')
+                    html = await main_view(lines, current)
+                    yield SSE.merge_signals({'current': 0, 'squares': ['', '', '', '', '', '']})
+                    yield SSE.merge_fragments(fragments=[html])
+                    game_over = await is_game_over(lines)
+                    match game_over:
+                        case "WIN":
+                            await asyncio.sleep(3)
+                            html = f"<main id='main' class='gc'>YAY, found the word in {current} tries!</main>"
+                            yield SSE.merge_fragments(fragments=[html])
+                        case "LOSE":
+                            await asyncio.sleep(3)
+                            html = f"<main id='main' class='gc'>:(, the word was {word}...</main>"
+                            yield SSE.merge_fragments(fragments=[html])
+                        case "NOPE":
+                            pass
+            except asyncio.CancelledError:
+                await pubsub.unsubscribe(str(db_id))
+                break
+    return await make_datastar_response(event())
+
+@app.post('/attempt')
+async def attempt():
+    squares = (await request.json).get('squares')
+    db_id = session['db_id']
+    data = games.get(doc_id=db_id)
+    word = data['word']
+    if squares:
+        diffs = []
+        for good_letter, square in zip(word, squares):
+            if good_letter == square:
+                diffs += [[good_letter, 'green']]
+            elif square in word:
+                diffs += [[square, 'yellow']]
+            else:
+                diffs += [[square, 'grey']]
+        current = data['current']
+        current_lines = data['lines']
+        current_lines[str(current)] = diffs
+        games.update(fields={'lines': current_lines, 'current': current+1}, doc_ids=[db_id])
+        await redis_client.publish(str(db_id), "ping")
+    return "", 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ static/js/main.js
@@ -0,0 +1,28 @@
+function react_to_key(evt, current, squares) {
+    // Handle letter input - any letter from A-Z
+    if (/^[A-Za-z]$/.test(evt.key)) {
+        if (current < 6) {
+            squares[current] = evt.key.toUpperCase();
+            current++;
+        }
+        return ['CONTINUE', [current, squares]];
+    }
+    
+    // Handle backspace
+    if (evt.key === 'Backspace') {
+        if (current > 0) {
+            current--;
+            squares[current] = '';
+        }
+        return ['CONTINUE', [current, squares]];
+    }
+    
+    // Handle enter
+    if (evt.key === 'Enter') {
+        if (current === 6) {
+            return ['OK', [current, squares]];
+        }
+    }
+    
+    return ['CONTINUE', [current, squares]];
+}
